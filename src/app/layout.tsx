42fdccea5104aeb5b56d226346eb4ea2d5f390a4
Truong Minh Phat <tmphat1312@gmail.com>
2024-12-04 06:42:46 +0000

    feature: add app temporary favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="shortcut icon"
+          href="https://fav.farm/🎞️"
+          type="image/x-icon"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
