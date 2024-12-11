8793b2c94aa2fb0e1738519705ceaa9652759132
Truong Minh Phat <tmphat1312@gmail.com>
2024-12-11 12:46:02 +0000

    func: apply navigation from home page to search page on search form

---
+++ src/features/home/components/search-movie-form.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Kbd } from "@/components/kbd";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+
+export function SearchMovieForm() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    function handleFocus(e: KeyboardEvent) {
+      if (e.key === "/") {
+        e.preventDefault();
+        inputRef.current?.focus();
+      }
+    }
+
+    window.addEventListener("keydown", handleFocus);
+
+    return () => {
+      window.removeEventListener("keydown", handleFocus);
+    };
+  });
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    router.push(`/search?query=${searchQuery}`);
+  }
+
+  return (
+    <form
+      className="flex items-center gap-2 rounded-full bg-background ps-2 text-foreground shadow-md outline-offset-2 has-[input:focus]:outline"
+      onSubmit={handleSubmit}
+    >
+      <input
+        ref={inputRef}
+        className="grow bg-transparent px-4 py-2.5 focus:outline-none"
+        placeholder="Search for a movie with a keyword or two :)..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <Kbd>/</Kbd>
+      <button
+        className="green-gradient self-stretch rounded-full px-6 disabled:pointer-events-none disabled:opacity-80"
+        disabled={searchQuery.length === 0}
+      >
+        Search
+      </button>
+    </form>
+  );
+}
