01e380d325a21b3f511f372501946be40cff32a3
Truong Minh Phat <tmphat1312@gmail.com>
2024-12-11 12:12:20 +0000

    refactor: use superstruct api to create typesafe move details data

--- src/data/get-movie-detail.ts
+++ src/data/get-movie-detail.ts
@@ -1,4 +1,6 @@
 import { apiClient } from "@/lib/apiClient";
+import { SMovieDetailsHttpResponse } from "@/types/http/movie-detail";
+import { mask } from "superstruct";
 
 type Options = {
   movie_id: number;
@@ -6,6 +8,7 @@ type Options = {
 
 export async function getMovieDetail({ movie_id }: Options) {
   const response = await apiClient.get(`/movie/${movie_id}`);
+  const typedResponse = mask(response.data, SMovieDetailsHttpResponse);
 
-  return response.data;
+  return typedResponse;
 }
