6d785a6c4ba3c1ee50219adc9bf9354c3b5edf12
Truong Minh Phat <tmphat1312@gmail.com>
2024-12-11 12:14:12 +0000

    ui: create a section placeholder to make the website scrollable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SearchHeroSection } from "@/features/home/components/search-hero-section";
 import { TrendingMoviesSection } from "@/features/home/components/trending-movies-section";
+import { SectionPlaceholder } from "@/components/section-placeholder";
 
 export default async function Home() {
   return (
@@ -7,7 +8,7 @@ export default async function Home() {
       <h1 className="text-bold sr-only mb-4 text-2xl">Home</h1>
       <SearchHeroSection />
       <TrendingMoviesSection />
-      <div className="h-[800px]"></div>
+      <SectionPlaceholder />
     </main>
   );
 }
