c39b9eb9307520147c3eeda29d8f2b273d65cc99
Truong Minh Phat <tmphat1312@gmail.com>
2024-12-11 12:15:20 +0000

    feat: display required information in details page

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,3 +1,22 @@
-export default function Id() {
-  return <div>Id</div>;
+import { Banner } from "@/features/movie-details/components/banner";
+import { MainInformation } from "@/features/movie-details/components/main-information";
+import { SectionPlaceholder } from "@/components/section-placeholder";
+
+export default async function MovieDetails({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const movieId = parseInt(id, 10);
+
+  return (
+    <>
+      <Banner movieId={movieId} />
+      <main>
+        <MainInformation movieId={movieId} />
+        <SectionPlaceholder />
+      </main>
+    </>
+  );
 }
