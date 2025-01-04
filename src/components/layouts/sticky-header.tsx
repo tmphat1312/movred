e54b3bb2fa9088bdc903cf38d8f91ae7852640c5
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-04 10:20:02 +0000

    refactor: rename filename to match kebab case

--- src/components/layouts/sticky-header.tsx
+++ src/components/layouts/sticky-header.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "../Logo";
+import { Logo } from "../logo";
 import { AuthenticationNavigation } from "./authentication-navigation";
 import { MainNavigation } from "./main-navigation";
 
