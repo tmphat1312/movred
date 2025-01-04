871bf3a52ca44f17b335debb97240ec252698501
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-04 13:20:53 +0000

    style: make the transition animation smooth

--- src/features/home/components/time-window-toggle.tsx
+++ src/features/home/components/time-window-toggle.tsx
@@ -20,17 +20,18 @@ function TimeWindowToggleWithoutSuspense() {
     <div className="relative overflow-clip rounded-full border border-layout-bg bg-transparent font-medium leading-5">
       <div
         className={cn(
-          "duration-400 absolute top-0 -z-10 h-full w-[82px] rounded-full bg-layout-bg transition-[left_width] ease-linear",
+          "absolute top-0 -z-10 h-full w-[82px] rounded-full bg-layout-bg transition-[left_width] duration-300 ease-linear",
           currentTimeWindow == "day" && "left-0 w-[82px]",
-          currentTimeWindow == "week" && "left-[82px] w-[108px]",
+          currentTimeWindow == "week" && "left-[82px] w-[120px]",
         )}
       />
       {possibleTimeWindows.map(({ value, label }) => (
         <button
           key={value}
           className={cn(
-            "rounded-full px-5 py-1 transition-colors",
+            "rounded-full px-5 py-1 font-bold transition-colors",
             currentTimeWindow == value && "text-green-gradient outline-red-500",
+            "active:green-gradient",
           )}
           onClick={() => changeTimeWindow(value)}
         >
