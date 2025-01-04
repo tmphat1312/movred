c8357b53807586ae8c7d41d1f3542d6c15460a1f
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-04 10:57:03 +0000

    ui: add a fancy logo for footer :))

---
+++ src/components/fancy-logo.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+
+export function FancyLogo() {
+  return (
+    <Link href="/" className="uppercase tracking-widest">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 280 120"
+        width={280}
+        height={120}
+      >
+        <defs>
+          <linearGradient id="__logo-gradient">
+            <stop offset="5%" stopColor="#89cda4" />
+            <stop offset="95%" stopColor="#13b6dc" />
+          </linearGradient>
+          <linearGradient id="__logo-gradient-reverse">
+            <stop offset="5%" stopColor="#13b6dc" />
+            <stop offset="95%" stopColor="#89cda4" />
+          </linearGradient>
+          <linearGradient id="__logo-gradient-circle">
+            <stop stopColor="#89cda4" />
+            <stop offset="80%" stopColor="#13beac" />
+          </linearGradient>
+          <linearGradient id="__logo-gradient-circle-reverse">
+            <stop stopColor="#13beac" />
+            <stop offset="80%" stopColor="#89cda4" />
+          </linearGradient>
+        </defs>
+        <text
+          x="0%"
+          y="50%"
+          fill="url(#__logo-gradient)"
+          fontSize="28"
+          fontWeight="bold"
+          alignmentBaseline="central"
+        >
+          MOVIES REC
+        </text>
+        <circle
+          cx="11.25%"
+          cy="50%"
+          r="10"
+          fill="url(#__logo-gradient-circle)"
+        />
+        <rect
+          x="57%"
+          y="50%"
+          transform="translate(4, -9)"
+          width={60}
+          height={18}
+          fill="#13b6dc"
+          rx="10"
+          ry="10"
+        />
+
+        <text
+          x="20%"
+          y="75%"
+          fill="url(#__logo-gradient-reverse)"
+          fontSize="28"
+          fontWeight="bold"
+          alignmentBaseline="central"
+        >
+          MENDATIONS
+        </text>
+        <circle
+          cx="67%"
+          cy="75%"
+          r="10"
+          fill="url(#__logo-gradient-circle-reverse)"
+        />
+        <rect
+          x="0%"
+          y="75%"
+          transform="translate(4, -9)"
+          width={48}
+          height={18}
+          fill="#13b6dc"
+          rx="10"
+          ry="10"
+        />
+      </svg>
+    </Link>
+  );
+}
--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,10 +1,11 @@
+import { FancyLogo } from "../fancy-logo";
+
 export function Footer() {
   return (
     <div className="bg-layout-bg text-layout-fg">
-      <footer className="container py-8">
-        <p className="text-green-gradient text-center text-3xl font-bold">
-          Movred &copy;2024
-        </p>
+      <footer className="container flex items-center justify-center gap-4 py-8">
+        <FancyLogo />
+        <p>Movred &copy;2024</p>
       </footer>
     </div>
   );
