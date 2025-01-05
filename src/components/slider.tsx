a1b44c98ab310307a5118d43715935d6052a0329
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-05 13:42:15 +0000

    style: add more gap to slide items

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -11,7 +11,7 @@ export function Slider({ className, ...props }: React.ComponentProps<"ul">) {
   return (
     <ul
       className={cn(
-        "scroll-snap custom-scrollbar flex snap-mandatory space-x-4 overflow-x-scroll whitespace-nowrap pb-6",
+        "scroll-snap custom-scrollbar flex snap-mandatory space-x-5 overflow-x-scroll whitespace-nowrap pb-6",
         className,
       )}
       {...props}
