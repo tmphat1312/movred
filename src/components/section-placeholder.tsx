07d6005539b9076c0151612ac1d6d5f55a0364cc
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-06 04:54:05 +0000

    style: make the section placeholder look cooler

--- src/components/section-placeholder.tsx
+++ src/components/section-placeholder.tsx
@@ -1,7 +1,11 @@
 export function SectionPlaceholder() {
   return (
-    <div className="container grid h-[600px] place-content-center text-2xl">
-      This is a placeholder section
-    </div>
+    <section aria-hidden="true" className="my-8">
+      <div className="container relative grid h-[400px] place-content-center border-b-4 border-t-4 border-layout-bg">
+        <div className="absolute -bottom-[16px] -top-[16px] left-[16px] w-[8px] bg-layout-bg" />
+        <div className="absolute -bottom-[16px] -top-[16px] right-[16px] w-[8px] bg-layout-bg" />
+        <h2 className="font-mono text-2xl">This is a placeholder section</h2>
+      </div>
+    </section>
   );
 }
