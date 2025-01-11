b40c41cda3f57565edf748d2275ffd243d3c15c8
Nguyen Phuc Phat <nguyenphucphat111999@gmail.com>
2025-01-11 17:33:35 +0700

    feat: import data from json to turso

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,9 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
 
-const sqlite = new Database("movred.sqlite");
-export const db = drizzle(sqlite);
+const client = createClient({
+  url: process.env.DATABASE_URL || "",
+  authToken: process.env.DATABASE_AUTH_TOKEN || "",
+});
+
+export const db = drizzle({ client });
