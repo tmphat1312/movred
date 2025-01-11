c332072296effcbb1b589cdc5a11d484a696deff
Nguyen Phuc Phat <nguyenphucphat111999@gmail.com>
2025-01-11 12:59:46 +0000

    chore: handle unauthenticated state on add movie to watch and favorite list

--- src/features/movie-details/components/user-actions.tsx
+++ src/features/movie-details/components/user-actions.tsx
@@ -1,11 +1,22 @@
 import { Suspense } from "react";
 import { AddToWatchList } from "./add-to-watch-list";
 import { PlayModalButton } from "./play-modal-button";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { UnderlineLink } from "@/components/ui/underline-link";
 
 export function UserActions({ movieId }: { movieId: number }) {
   return (
     <div className="mb-2.5 flex items-center gap-4">
-      <AddToWatchList movieId={movieId} />
+      <SignedIn>
+        <AddToWatchList movieId={movieId} />
+      </SignedIn>
+      <SignedOut>
+        <div className="rounded-md bg-layout-bg px-2 py-1 text-sm">
+          <UnderlineLink href="/sign-in">
+            Sign in to add to watch and favorite lists
+          </UnderlineLink>
+        </div>
+      </SignedOut>
       <Suspense>
         <PlayModalButton movieId={movieId} />
       </Suspense>
