23556ff40dff2e4151e7b9d9768f330666611d57
Nguyen Phuc Phat <nguyenphucphat111999@gmail.com>
2025-01-11 13:47:53 +0000

    chore: add rating form ui

--- src/features/movie-details/components/rating.tsx
+++ src/features/movie-details/components/rating.tsx
@@ -1,3 +1,25 @@
+import { ScaleSlider } from "@/components/ui/scale-slider";
+
 export function Rating() {
-  return <div>Rating</div>;
+  const yourRating = undefined;
+
+  if (yourRating) {
+    return (
+      <p>
+        Your rating: <span className="text-lg font-bold">{yourRating}</span>
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <h4 className="mb-2 font-semibold">What is your score for this movie.</h4>
+      <div className="flex items-center gap-5">
+        <ScaleSlider />
+        <button className="light-blue-gradient hover:green-gradient rounded-full px-4 py-2">
+          Okay, Save My Rating
+        </button>
+      </div>
+    </div>
+  );
 }
