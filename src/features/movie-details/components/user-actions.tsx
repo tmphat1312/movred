4a005ec65b46ea683a998f5b76a840f7f81fb09e
Nguyen Phuc Phat <nguyenphucphat111999@gmail.com>
2025-01-11 12:56:23 +0000

    chore: create add to watch list ui

--- src/features/movie-details/components/user-actions.tsx
+++ src/features/movie-details/components/user-actions.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from "react";
+import { AddToWatchList } from "./add-to-watch-list";
 import { PlayModalButton } from "./play-modal-button";
 
 export function UserActions({ movieId }: { movieId: number }) {
   return (
-    <div className="h-[60px]">
+    <div className="mb-2.5 flex items-center gap-4">
+      <AddToWatchList movieId={movieId} />
       <Suspense>
         <PlayModalButton movieId={movieId} />
       </Suspense>
