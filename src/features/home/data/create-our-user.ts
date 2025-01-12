b7fb4eaba86b0228aed8193a91671010f98764d6
Tran Duc Minh <flashback054@gmail.com>
2025-01-12 23:11:16 +0000

    chore: add fullname to our internal user

--- src/features/home/data/create-our-user.ts
+++ src/features/home/data/create-our-user.ts
@@ -1,6 +1,13 @@
+import { clerkClient } from "@clerk/nextjs/server";
+
 import { db } from "@/data/db";
 import { users } from "@/data/schema";
 
 export async function createOurUser({ clerkId }: { clerkId: string }) {
-  await db.insert(users).values({ clerk_id: clerkId });
+  const client = await clerkClient();
+  const user = await client.users.getUser(clerkId);
+
+  await db
+    .insert(users)
+    .values({ clerk_id: clerkId, fullname: user.fullName || user.username });
 }
