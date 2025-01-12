439668558e204f2deeb20fb43cde12c3150d796b
Nguyen Phuc Phat <nguyenphucphat111999@gmail.com>
2025-01-12 10:34:31 +0000

    chore: remove column name alias and add trending movies table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,16 @@
 import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
-  id: integer("id").primaryKey(),
-  clerk_id: text("clerk_id"),
+  id: integer().primaryKey({ autoIncrement: true }),
+  clerk_id: text().notNull(),
+});
+
+export const trending_movies = sqliteTable("trending_movies", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  title: text().notNull(),
+  release_date: text(),
+  poster_path: text(),
+  vote_average: text(),
+  tmdb_id: integer().notNull(),
+  time_window: text({ enum: ["day", "week"] }).notNull(),
 });
