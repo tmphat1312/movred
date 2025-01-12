743440ba5faf4d587b501b218ead40a61733594b
Nguyen Phuc Phat <nguyenphucphat111999@gmail.com>
2025-01-12 04:20:30 +0000

    qa: disable eslint

--- src/db/import_data.ts
+++ src/db/import_data.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const fs = require("fs");
 const { chain } = require("stream-chain");
 const { parser } = require("stream-json");
