405553baa5b73cef77e9962b3a18ac9030217de2
Tran Duc Minh <flashback054@gmail.com>
2025-01-12 23:11:27 +0000

    feat: view reviews list close (#35)

--- src/features/movie-details/data/get-movie-reviews.ts
+++ src/features/movie-details/data/get-movie-reviews.ts
@@ -1,7 +1,16 @@
 import { db } from "@/data/db";
-import { reviews } from "@/data/schema";
+import { reviews, users } from "@/data/schema";
 import { eq } from "drizzle-orm";
 
 export async function getMovieReviews({ movieId }: { movieId: number }) {
-  return await db.select().from(reviews).where(eq(reviews.movie_id, movieId));
+  return await db
+    .select({
+      review: reviews.review,
+      from: users.fullname,
+      movieId: reviews.movie_id,
+      userId: reviews.user_id,
+    })
+    .from(reviews)
+    .leftJoin(users, eq(reviews.user_id, users.id))
+    .where(eq(reviews.movie_id, movieId));
 }
