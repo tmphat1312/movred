d065621cffe89aa4dfe880061a52c1dd36a1ee4c
Tran Duc Minh <flashback054@gmail.com>
2025-01-12 23:16:32 +0000

    chore: update user infor on login

--- src/features/home/components/create-our-user.tsx
+++ src/features/home/components/create-our-user.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { findUserByClerkId } from "../data/find-user-by-clerk-id";
 import { createOurUser } from "../data/create-our-user";
+import { updateOurUser } from "../data/update-our-user";
 
 export async function CreateOurUser() {
   const { userId } = await auth();
@@ -11,6 +12,8 @@ export async function CreateOurUser() {
 
   if (!internalUser) {
     await createOurUser({ clerkId: userId });
+  } else {
+    await updateOurUser({ clerkId: userId });
   }
 
   return null;
