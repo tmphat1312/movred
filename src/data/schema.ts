49624f261a86af2e4490e94d0988a78b03c07df5
Tran Duc Minh <flashback054@gmail.com>
2025-01-12 22:48:49 +0000

    chore: add a fullname prop to `users` table

--- src/data/schema.ts
+++ src/data/schema.ts
@@ -10,6 +10,7 @@ import {
 export const users = sqliteTable("users", {
   id: integer().primaryKey({ autoIncrement: true }),
   clerk_id: text().notNull(),
+  fullname: text(),
 });
 
 export const trending_movies = sqliteTable("trending_movies", {
