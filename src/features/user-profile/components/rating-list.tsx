d8dbccf4c29609258859b3ee0d663f07dbb57652
Tran Duc Minh <flashback054@gmail.com>
2025-01-13 05:58:16 +0000

    ui: create draft ui layout for rating list

--- src/features/user-profile/components/rating-list.tsx
+++ src/features/user-profile/components/rating-list.tsx
@@ -1,3 +1,15 @@
-export function RatingList() {
+import { getRatingList } from "../data/get-rating-list";
+
+export async function RatingList() {
+  const list = await getRatingList();
+
+  if (!list || list.length === 0) {
+    return (
+      <p className="text-green-gradient text-lg">
+        You have not rated any movie yet.
+      </p>
+    );
+  }
+
   return <div>RatingList</div>;
 }
