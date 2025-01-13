db3fefdbc06a4ca6f6fa2193314c316fd30f0a13
Tran Duc Minh <flashback054@gmail.com>
2025-01-13 01:20:55 +0000

    refactor: use get internal user instead

--- src/features/movie-details/data/is-movie-in-your-favorite-list.ts
+++ src/features/movie-details/data/is-movie-in-your-favorite-list.ts
@@ -1,17 +1,19 @@
+import { and, eq } from "drizzle-orm";
+
 import { db } from "@/data/db";
 import { favorites } from "@/data/schema";
-import { and, eq } from "drizzle-orm";
+import { getInternalUser } from "@/lib/data/get-internal-user";
 
 export async function isMovieInYourFavoriteList({
   movieId,
-  userId,
 }: {
   movieId: number;
-  userId: number;
 }) {
+  const { id: userId } = await getInternalUser();
   const list = await db
     .select()
     .from(favorites)
     .where(and(eq(favorites.movie_id, movieId), eq(favorites.user_id, userId)));
+
   return list.length > 0;
 }
