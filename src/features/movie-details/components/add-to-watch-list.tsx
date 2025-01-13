8cb8afefe0dac02179daeb0743fe5104d34a9eff
Tran Duc Minh <flashback054@gmail.com>
2025-01-13 04:02:55 +0000

    fix: misuse the function to check favorite list

--- src/features/movie-details/components/add-to-watch-list.tsx
+++ src/features/movie-details/components/add-to-watch-list.tsx
@@ -1,9 +1,9 @@
-import { isMovieInYourFavoriteList } from "../data/is-movie-in-your-favorite-list";
+import { isMovieInYourWatchList } from "../data/is-movie-in-your-watch-list";
 import { AddToWatchListForm } from "./add-to-watch-list-form";
 import { RemoveFromWatchListForm } from "./remove-from-watch-list-form";
 
 export async function AddToWatchList({ movieId }: { movieId: number }) {
-  const isInWatchList = await isMovieInYourFavoriteList({ movieId });
+  const isInWatchList = await isMovieInYourWatchList({ movieId });
 
   return (
     <>
